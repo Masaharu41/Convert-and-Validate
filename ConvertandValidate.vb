62e5acd304571865732406b1ca1121ae90a63a43
Owen Fujii <dalekrebel@gmail.com>
2024-02-07 08:07:08 -0700

    Added full code and leave function

--- ConvertandValidate.vb
+++ ConvertandValidate.vb
@@ -2,42 +2,52 @@
 'RCET 2265
 'Spring 2024
 'Convert and Validate
-'
+'https://github.com/Masaharu41/Convert-and-Validate.git
 
 Option Explicit On
 Option Strict On
+Option Compare Text
 Module ConvertandValidate
 
     Sub Main()
 
-        'for testing the function add this to your Sub Main()
+        'Convert the user inputed string to a integer and validates the converstion
 
         Dim aValidNumber As Integer
 
         Do
             Dim usernResponse As String
-
+            Console.WriteLine("Press q at anytime to leave")
             Console.WriteLine($"Enter a number:")
 
             usernResponse = Console.ReadLine()
-
-            If ConversionValid(usernResponse, aValidNumber) = True Then
-
-                Console.WriteLine($"{usernResponse} converted successfully to {aValidNumber}!")
-
+            If usernResponse = "q" Then
+                Exit Do
             Else
+                If ConversionValid(usernResponse, aValidNumber) = True Then
 
-                Console.WriteLine($"Oops, {usernResponse} does not seem to be a number")
+                    Console.WriteLine($"{usernResponse} converted successfully to {aValidNumber}!")
 
+                Else
+
+                    Console.WriteLine($"Oops, {usernResponse} does not seem to be a number")
+
+                End If
             End If
-
         Loop
+        Console.WriteLine("Have a Nice Day!")
         Console.Read()
     End Sub
-    'make this work
 
 
-    Function ConversionValid(convertThisString$, toThisInteger As Integer) As Boolean
+    ''' <summary>
+    ''' Funtion that takes the user input and completes convertion
+    ''' while returning a boolean statement for failed or passed convertions
+    ''' </summary>
+    ''' <param name="convertThisString$"></param>
+    ''' <param name="toThisInteger%"></param>
+    ''' <returns></returns>
+    Function ConversionValid(convertThisString$, ByRef toThisInteger%) As Boolean
 
         Dim status As Boolean
 
@@ -46,9 +56,7 @@ Module ConvertandValidate
             status = True
         Catch ex As Exception
             status = False
-
         End Try
-
         Return status
 
     End Function
